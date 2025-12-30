d0dac5b347d57ca2f7a64e7ba5991577489624c7
ananyabhardwaj10 <ananyabhardwaj0510@gmail.com>
2025-12-30 14:16:15 +0000

    adding the docs folder

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/ananyabhardwaj10/static_site_generator/"
--- src/extract_title.py
+++ src/extract_title.py
@@ -1,5 +1,6 @@
 from blocks import markdown_to_html_node
 import os
+from pathlib import Path
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -8,7 +9,7 @@ def extract_title(markdown):
             return line.strip("#").strip()
     raise Exception("Invalid Heading. Provide a valid heading.")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         markdown = f.read()
@@ -25,6 +26,8 @@ def generate_page(from_path, template_path, dest_path):
 
     full_html = template.replace("{{ Title }}", title)
     full_html = full_html.replace("{{ Content }}", html_content)
+    full_html = full_html.replace('href="/', 'href="' + basepath)
+    full_html = full_html.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -33,34 +36,14 @@ def generate_page(from_path, template_path, dest_path):
     to_file.write(full_html)
     to_file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
-        full_path = os.path.join(dir_path_content, filename)
-
-        if os.path.isfile(full_path):
-            with open(full_path) as f:
-                markdown = f.read()
-            f.closed
-
-            html_node = markdown_to_html_node(markdown)
-            html_content = html_node.to_html() 
-
-            with open(template_path) as f:
-                template = f.read()
-
-            name, ext = os.path.splitext(filename)
-            dest_file_path = os.path.join(dest_dir_path, name + ".html")
-            title = extract_title(markdown)
-
-            full_html = template.replace("{{ Title }}", title)
-
-            full_html = full_html.replace("{{ Content }}", html_content)
-
-            with open(dest_file_path, "w") as f:
-                f.write(full_html)
-
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+            
         else:
-            dest_subdir = os.path.join(dest_dir_path, filename)
-            os.makedirs(dest_subdir, exist_ok=True)
-            generate_pages_recursive(full_path, template_path, dest_subdir)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
         
